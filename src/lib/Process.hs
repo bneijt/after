401b2548a4fb3e827c8ccdf27a9af4e2c07ea8c1
A. Bram Neijt <bneijt@gmail.com>
2014-10-16 15:39:38 +0200

    Touch up documentation

--- src/lib/Process.hs
+++ src/lib/Process.hs
@@ -3,12 +3,14 @@ import System.Directory (getDirectoryContents)
 import System.FilePath ((</>))
 import Data.Char (isDigit)
 
--- Filter directories that contain numbers
+-- Filter /proc directories that contain numbers
 psListing :: IO [FilePath]
 psListing = do
     listing <- getDirectoryContents "/proc"
     return $ filter (all isDigit :: FilePath -> Bool) listing
 
+-- Load the /proc/<pid>/cmdline contents
+-- concats arguments using ' ' (argument boundry information is lost!)
 psCmdLine :: String -> IO String
 psCmdLine pid = do
     line <- readFile ("/proc" </> pid </> "cmdline")
