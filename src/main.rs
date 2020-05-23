1b1a6db44c42047b119f4734eeefbf261a160e14
A. Bram Neijt <bram@neijt.nl>
2020-05-23 16:46:17 +0200

    Add documentation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+/*! Commandline application to wait for another process to stop.
+
+ After you started a long running command in one terminal, you decide to want
+ to run another command after the first command is finished.
+
+ This is where `after` will help. You simply give it some words of the
+ command you started and it will look up the pid and wait for it to exit.
+
+ ## Example
+
+ Open a terminal with `sleep 1m` then open another terminal and
+ type `after sleep; echo Done`.
+
+ The `after` command will now wait for the `sleep` command to exit and exit
+ itself, allow `echo Done` to be executed.
+
+*/
+
 use clap::{App, Arg};
 use libc::pid_t;
 use procfs::process::Process;
@@ -5,14 +23,16 @@ use std;
 use std::fs;
 use std::io;
 use std::path::PathBuf;
-use std::{thread, time};
 use std::process;
+use std::{thread, time};
 
+/// Struct to collect information on the process
 struct ProcessCommandline {
     process_id: pid_t,
     commandline: String,
 }
 
+///Get a list of other pids running on the system
 fn other_pids() -> io::Result<Vec<pid_t>> {
     let mut pids: Vec<pid_t> = Vec::new();
     let self_pid = process::id() as pid_t;
@@ -38,6 +58,7 @@ fn other_pids() -> io::Result<Vec<pid_t>> {
     Ok(pids)
 }
 
+///Get the commandline of the given process (if allowed and succesfull)
 fn get_cmdline(pid: &pid_t) -> Option<ProcessCommandline> {
     match Process::new(*pid) {
         Ok(process) => match process.cmdline() {
@@ -52,19 +73,21 @@ fn get_cmdline(pid: &pid_t) -> Option<ProcessCommandline> {
     }
 }
 
+///Given the words to match and the process description, see if the process matches
 fn select_pid(match_words: &Vec<String>, process_commandline: &ProcessCommandline) -> bool {
     let sentence = match_words.join(" ");
     process_commandline.commandline.contains(sentence.as_str())
 }
 
+///Main entrypoint for the commandline interface
 fn main() {
     let matches = App::new("after")
-        .version("0.1.0")
-        .author("Bram <bram@neijt.nl>")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
         .about("Exit after other commands are done")
         .arg(
             Arg::with_name("PROC")
-                .help("Process description to wait for")
+                .help("Parts of the commandline of the process you want to wait for")
                 .multiple(true),
         )
         .get_matches();
@@ -81,7 +104,10 @@ fn main() {
                 .filter(|pc| select_pid(&match_words, pc))
                 .collect();
             for process_commandline in &wait_list {
-                println!("wait for: {}", process_commandline.commandline);
+                println!(
+                    "waiting for {}: {}",
+                    process_commandline.process_id, process_commandline.commandline
+                );
             }
             for process_commandline in &wait_list {
                 let mut proc_path = PathBuf::new();
@@ -90,11 +116,11 @@ fn main() {
                 while proc_path.as_path().exists() {
                     thread::sleep(time::Duration::from_millis(500));
                 }
-                println!("after: {}", process_commandline.commandline);
+                println!("{} is closed", process_commandline.process_id);
             }
         }
         None => {
-            println!("No process description given, try --help maybe?");
+            println!("Missing required command description");
             std::process::exit(1);
         }
     }
